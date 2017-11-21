c571cdc80e50cd1e899f541644606afbd6e6e806
MeijerM1 <max1_meijer@hotmail.com>
2017-11-21 17:43:36 +0100

    Added new project

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package repository;
+
+import java.util.List;
+
+import models.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    List<User> findByLastName(@Param("name") String name);
+
+}
