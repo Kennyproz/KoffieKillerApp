2a483a243002638bbb434c8c17e073cb34c292eb
MeijerM1 <max1_meijer@hotmail.com>
2017-11-21 11:37:07 +0100

    Updated stuff

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,6 +1,19 @@
 package Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@Entity
 public class User {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) private Long id;
 
     private String username;
 
