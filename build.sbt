0919694efbbcc5abca89ab42e4703a4a899cb0bd
MeijerM1 <max1_meijer@hotmail.com>
2017-12-05 11:37:41 +0100

    Aids

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """play-java-starter-example"""
+name := """play-java-jpa-example"""
 
 version := "1.0-SNAPSHOT"
 
@@ -7,13 +7,22 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.12.2"
 
 libraryDependencies += guice
-
-// Test Database
+libraryDependencies += javaJpa
+val appDependencies = Seq(
+  "mysql" % "mysql-connector-java" % "5.1.45"
+)
 libraryDependencies += "com.h2database" % "h2" % "1.4.194"
+libraryDependencies += "org.hibernate" % "hibernate-core" % "5.2.5.Final"
+libraryDependencies ++= Seq(
+  javaJpa,
+  "org.hibernate" % "hibernate-entitymanager" % "5.2.5.Final" // replace by your jpa implementation
 
-// Testing libraries for dealing with CompletionStage...
-libraryDependencies += "org.assertj" % "assertj-core" % "3.6.2" % Test
-libraryDependencies += "org.awaitility" % "awaitility" % "2.0.0" % Test
+)
 
-// Make verbose tests
-testOptions in Test := Seq(Tests.Argument(TestFrameworks.JUnit, "-a", "-v"))
+libraryDependencies += javaWs % "test"
+
+libraryDependencies += "org.awaitility" % "awaitility" % "2.0.0" % "test"
+libraryDependencies += "org.assertj" % "assertj-core" % "3.6.2" % "test"
+libraryDependencies += "org.mockito" % "mockito-core" % "2.1.0" % "test"
+testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-a", "-v")
+
--- test/UnitTest.java
+++ test/UnitTest.java
@@ -1,55 +1,102 @@
-import akka.actor.ActorSystem;
-import controllers.AsyncController;
-import controllers.CountController;
+import controllers.PersonController;
+import models.Person;
+import models.PersonRepository;
 import org.junit.Test;
+import play.core.j.JavaContextComponents;
+import play.core.j.JavaHelpers;
+import play.data.FormFactory;
+import play.data.format.Formatters;
+import play.i18n.MessagesApi;
+import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
+import play.mvc.Http;
 import play.mvc.Result;
-import scala.concurrent.ExecutionContextExecutor;
+import play.test.Helpers;
+import play.twirl.api.Content;
 
+import javax.validation.Validator;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ForkJoinPool;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.*;
 import static org.awaitility.Awaitility.await;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static play.mvc.Http.Status.OK;
+import static play.mvc.Http.Status.SEE_OTHER;
 import static play.test.Helpers.contentAsString;
 
 /**
- * Unit testing does not require Play application start up.
- *
+ * Simple (JUnit) tests that can call all parts of a play app.
+ * <p>
  * https://www.playframework.com/documentation/latest/JavaTest
  */
-public class UnitTest {
+public class UnitTest implements JavaHelpers {
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void checkIndex() {
+        // XXX This is a gap in the test API -- it should be play.test.Helpers.httpContext() by 2.6.0-M4
+        // and JavaHelpers should be removed.
+        Http.RequestBuilder request = Helpers.fakeRequest("GET", "/");
+
+        // XXX This should be play.test.CSRFTokenHelper
+        Http.RequestBuilder tokenRequest = play.api.test.CSRFTokenHelper.addCSRFToken(request);
+        JavaContextComponents contextComponents = createContextComponents();
+        Http.Context.current.set(createJavaContext(tokenRequest.build()._underlyingRequest(), contextComponents));
+
+        PersonRepository repository = mock(PersonRepository.class);
+        FormFactory formFactory = mock(FormFactory.class);
+        HttpExecutionContext ec = new HttpExecutionContext(ForkJoinPool.commonPool());
+        final PersonController controller = new PersonController(formFactory, repository, ec);
+        final Result result = controller.index();
+
+        assertThat(result.status()).isEqualTo(OK);
     }
 
-    // Unit test a controller
     @Test
-    public void testCount() {
-        final CountController controller = new CountController(() -> 49);
-        Result result = controller.count();
-        assertThat(contentAsString(result)).isEqualTo("49");
+    public void checkTemplate() {
+        Content html = views.html.index.render();
+        assertThat(html.contentType()).isEqualTo("text/html");
+        assertThat(contentAsString(html)).contains("Add Person");
     }
 
-    // Unit test a controller with async return
     @Test
-    public void testAsync() {
-        final ActorSystem actorSystem = ActorSystem.create("test");
-        try {
-            final ExecutionContextExecutor ec = actorSystem.dispatcher();
-            final AsyncController controller = new AsyncController(actorSystem, ec);
-            final CompletionStage<Result> future = controller.message();
+    public void checkAddPerson() {
+        // Easier to mock out the form factory inputs here
+        MessagesApi messagesApi = mock(MessagesApi.class);
+        Validator validator = mock(Validator.class);
+        FormFactory formFactory = new FormFactory(messagesApi, new Formatters(messagesApi), validator);
 
-            // Block until the result is completed
-            await().until(() -> {
-                assertThat(future.toCompletableFuture()).isCompletedWithValueMatching(result -> {
-                    return contentAsString(result).equals("Hi!");
-                });
-            });
-        } finally {
-            actorSystem.terminate();
-        }
+        // Don't need to be this involved in setting up the mock, but for demo it works:
+        PersonRepository repository = mock(PersonRepository.class);
+        Person person = new Person();
+        person.id = 1L;
+        person.name = "Steve";
+        when(repository.add(any())).thenReturn(supplyAsync(() -> person));
+
+        // Set up the request builder to reflect input
+        final Http.RequestBuilder requestBuilder = new Http.RequestBuilder().method("post").bodyJson(Json.toJson(person));
+
+        // Add in an Http.Context here using invokeWithContext:
+        // XXX extending JavaHelpers is a cheat to get at JavaContextComponents easily, put this into helpers
+        JavaContextComponents components = createContextComponents();
+        final CompletionStage<Result> stage = Helpers.invokeWithContext(requestBuilder, components, () -> {
+            HttpExecutionContext ec = new HttpExecutionContext(ForkJoinPool.commonPool());
+
+            // Create controller and call method under test:
+            final PersonController controller = new PersonController(formFactory, repository, ec);
+            return controller.addPerson();
+        });
+
+        // Test the completed result
+        await().atMost(1, SECONDS).until(() ->
+            assertThat(stage.toCompletableFuture()).isCompletedWithValueMatching(result ->
+                result.status() == SEE_OTHER, "Should redirect after operation"
+            )
+        );
     }
 
 }
