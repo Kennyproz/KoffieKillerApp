eb7ea713f2d29d7eecd8a7598e21e5e921cf685d
MeijerM1 <max1_meijer@hotmail.com>
2017-12-05 12:53:42 +0100

    works

--- app/models/Person.java
+++ app/models/Person.java
@@ -3,10 +3,11 @@ package models;
 import javax.persistence.*;
 
 @Entity
+@Table
 public class Person {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Long id;
 
     public String name;
