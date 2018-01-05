66ba03ef4bc1f9861c3fd1ef402d6abea404d20f
Kennyproz <k.odigwegomez@student.fontys.nl>
2018-01-05 16:52:56 +0100

    Bug fixes

--- public/javascripts/coffeeContent.js
+++ public/javascripts/coffeeContent.js
@@ -13,13 +13,13 @@ function setCoffeeContent(data) {
         var li = $('<li>').text(element.name).appendTo(container);
         var deleteButton = $('<button>').text(' delete').appendTo(li);
         deleteButton.bind('click', function() {
-            deletePerson(element.id);
+            deleteCoffee(element.id);
         })
 
         var editButton = $('<button>').text(' Edit').appendTo(li);
         editButton.bind('click', function() {
             var newName = $("#inputname")+element.id.val();
-            editPerson(element.id,newName);
+            editCoffee(element.id,newName);
         })
 
     });
@@ -30,13 +30,13 @@ function setCoffeeContent(data) {
 }
 
 
-function editPerson(id, newName) {
+function editCoffee(id, newName) {
     $.get('/editCoffee/' + id + '/' + newName, function (data) {
         getCoffee();
     });
 }
 
-function deletePerson(id) {
+function deleteCoffee(id) {
     $.get('/deleteCoffee/' + id, function (data) {
         getCoffee();
     });
